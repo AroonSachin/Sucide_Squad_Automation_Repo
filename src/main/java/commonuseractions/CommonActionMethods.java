fca3ea3468a4bd28af8c344d40aeffbd96474b85
vbaskar <vbaskar@tsqa-l062.ittrident.local>
2022-06-23 17:25:22 +0530

    using click method in commonaction method

--- src/main/java/commonuseractions/CommonActionMethods.java
+++ src/main/java/commonuseractions/CommonActionMethods.java
@@ -1,7 +1,48 @@
 package commonuseractions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CommonActionMethods {
 	
+	
+	
+		
+	
+	   
+	public static void clickMethod(WebElement elements,String value)  {
+		try {
+					
+			elements.click( );
+				
+			
+		} catch (Exception e) {
+			
+			
+			
+		}
+	}
+		public static  void sendKeysMethod(WebElement keys,String enter) {
+			try {
+				keys.click();
+				keys.sendKeys(enter);
+				
+				
+			} catch (Exception e) {
+				
+				
+				
+			}
+			
+			}
+		
+		
+		
+		
+}
 	
 	
 	
