05a147bacbb4765dbd81be8d71be7b60b0e79ac0
svenkateshwaran <svenkateshwaran@TSQA-L107.ittrident.local>
2022-06-30 21:57:27 +0530

    Excel reader to reader the data using data provider

---
+++ src/main/java/utils/ExcelReader.java
@@ -0,0 +1,53 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+public class ExcelReader
+{
+private static Map<String, String> dataMap = new HashMap<String, String>();
+
+public ExcelReader(String Excelpath, String sheetname, int rowNum) throws IOException {
+
+	dataMap.clear();
+	try {
+		FileInputStream fs = new FileInputStream(new File(Excelpath));
+		XSSFWorkbook workbook = new XSSFWorkbook(fs);                  //Read the excel work book
+		XSSFSheet sheet = workbook.getSheet(sheetname);                //read the excel sheet based on sheet name
+		String title = "";
+		String value = "";
+		
+
+		for (int j = 0; j < sheet.getRow(0).getLastCellNum(); j++) {
+			title = sheet.getRow(0).getCell(j).getStringCellValue();    //fetches the header name from the sheet
+			value = sheet.getRow(rowNum).getCell(j).getStringCellValue(); //fetches the value of the header from the sheet
+			dataMap.put(title, value);                                    //Stores the header name and the value in the map
+		}
+	} catch (Exception e) {
+		System.err.println("Error during reading input file");
+		throw new Error(e.getStackTrace().toString());
+	}
+}
+
+public static int getRowCount(String Excelpath, String sheetname) throws IOException
+{
+	FileInputStream fs = new FileInputStream(new File(Excelpath));
+	XSSFWorkbook workbook = new XSSFWorkbook(fs);                  //Read the excel work book
+	XSSFSheet sheet = workbook.getSheet(sheetname);
+	return sheet.getLastRowNum()+1;
+}
+
+public static Map<String, String> hashmap() {
+	return dataMap;
+}
+}
+	
+
--- src/test/java/swaglab/SwagLabTest.java
+++ src/test/java/swaglab/SwagLabTest.java
@@ -1,5 +1,37 @@
 package swaglab;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import utils.ExcelReader;
+
 public class SwagLabTest {
+	
+    @DataProvider(name="automation")
+	public static Iterator<Object[]> dataFetcher() throws IOException
+	{
+	ArrayList<Object[]> data = new ArrayList<Object[]>();
+    int count = ExcelReader.getRowCount(System.getProperty("user.dir")+"/src/main/java/resources/excelsheet/bio.xlsx", "Test");
+    for (int i = 1; i <count ; i++) {
+		data.add(new Object[] { new ExcelReader(System.getProperty("user.dir")+"/src/main/java/resources/excelsheet/bio.xlsx", "Test", i) });
+	    
+	}
+
+	return data.iterator();
+	}
+	
+	@Test(dataProvider="automation")
+	public void test1(ExcelReader data1) throws IOException
+	{
+		System.out.println(data1.hashmap().get("S.no."));
+		System.out.println(data1.hashmap().get("FirstName"));
+		System.out.println(data1.hashmap().get("LastName"));
+		System.out.println(data1.hashmap().get("DateOfBirth"));
+		System.out.println(data1.hashmap().get("Nationality"));
+	}
 
 }
