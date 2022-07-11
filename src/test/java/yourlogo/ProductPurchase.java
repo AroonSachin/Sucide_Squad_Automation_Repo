3cd06a39e57005394e283ebd2a2f01fcd74465b3
vbaskar <vbaskar@tsqa-l062.ittrident.local>
2022-07-11 18:34:55 +0530

    Adding the testcases in page object model

--- src/test/java/yourlogo/ProductPurchase.java
+++ src/test/java/yourlogo/ProductPurchase.java
@@ -1,7 +1,23 @@
 package yourlogo;
-public class ProductPurchase {
-	
-	
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import commonuseractions.CommonActionMethods;
+
+public class ProductPurchase extends CommonActionMethods {
+	
+	@BeforeMethod
+	
+	  public void bro() {
+		invokeBrowser("chrome", "normal", "http://automationpractice.com/index.php");
+	}
+
+@Test
+ private void meth() {
+	logMessage("hi");
+	logMessage(getTitle());
+	logMessage(getURL());
+	
+}
 }
