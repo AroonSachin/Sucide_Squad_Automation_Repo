8ee71c33ebbe1cda00fc4a47e4668c55c6d7878c
svenkateshwaran <svenkateshwaran@TSQA-L107.ittrident.local>
2022-07-12 15:39:48 +0530

    Java programs

---
+++ src/test/java/swaglab/Runner.java
@@ -0,0 +1,17 @@
+package swaglab;
+
+import org.testng.annotations.Test;
+
+import commonuseractions.CommonActionMethods;
+
+public class Runner extends CommonActionMethods{
+
+	@Test
+	public static void testCase()
+	{
+		//System.out.println("Hello");
+		logMessage("Hai");
+
+	}
+
+}
