f4ef20b739d83a515bf8b46fc1100801d106017c
vbaskar <vbaskar@tsqa-l062.ittrident.local>
2022-08-11 13:58:51 +0530

    updated retry analyser

--- src/test/java/yourlogo/YourLogo.java
+++ src/test/java/yourlogo/YourLogo.java
@@ -5,10 +5,8 @@ import java.util.Map;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import commonuseractions.CommonActionMethods;
