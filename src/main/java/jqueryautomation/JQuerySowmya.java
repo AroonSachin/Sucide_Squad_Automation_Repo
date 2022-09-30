af31a5c5c4be43bcfcc5c55bbcc04f390697e160
svenkateshwaran <svenkateshwaran@TSQA-L107.ittrident.local>
2022-09-30 11:01:52 +0530

    JQuery

--- src/main/java/jqueryautomation/JQuerySowmya.java
+++ src/main/java/jqueryautomation/JQuerySowmya.java
@@ -1,6 +1,7 @@
 package jqueryautomation;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -13,16 +14,42 @@ public class JQuerySowmya extends CommonActionMethods {
 	{
 		PageFactory.initElements(DriverFactory.getDriver(), this);
 	}
-	@FindBy(xpath = "//<a>[text()='Draggable']")
-	private static WebElement dragButton;
+	@FindBy(xpath = "//a[text()='Draggable']")
+	public WebElement dragButton;
+	
+	@FindBy(id="draggable") 
+	public  WebElement draggable;
+	
+	@FindBy(xpath = "//a[text()='Droppable']")
+	public WebElement dropButton;
 	
 	
 	
 	
-	public void draggable()
+	
+	public void draggable() throws InterruptedException
 	{
+		Thread.sleep(1000);
 		dragButton.click();
+		System.out.println("Draggable button is clicked");
 	}
+	
+	public void dragAndDrop() throws Exception {
+		frameByIndex(0);
+		webWait(draggable);
+		Actions drag=new Actions(DriverFactory.getDriver());
+		int x= draggable.getLocation().getX();
+		int y=draggable.getLocation().getY();
+		drag.dragAndDropBy(draggable, x+45,y+65).perform();
+		defaultwindow();
+	}
+	
+	public void droppable()
+	{
+		dropButton.click();
+		System.out.println("Droppable button is clicked");
+	}
+	
 	
 	
 
--- src/test/java/jqueryrunner/JQueryTest.java
+++ src/test/java/jqueryrunner/JQueryTest.java
@@ -1,21 +1,29 @@
 package jqueryrunner;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import commonuseractions.CommonActionMethods;
 import jqueryautomation.JQuerySowmya;
 
 public class JQueryTest extends CommonActionMethods {
-	
+	@BeforeMethod
 	public void startBrowser() throws Exception
 	{
 		invokeBrowser("chrome", "Windows", "https://jqueryui.com/");
 	}
 	@Test
-	public void test1()
+	public void test1() throws Exception
 	{
 		new JQuerySowmya().draggable();
+		new JQuerySowmya().dragAndDrop();
+		new JQuerySowmya().droppable();
 	}
+	
+//	public void test2() throws Exception
+//	{
+//		new JQuerySowmya().dragAndDrop();
+//	}
 	
 	
 
