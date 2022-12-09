bd6d22a83f3cec55acf44b4634ffbd16cf982c3f
Arun Sachin <107554732+AroonSachin@users.noreply.github.com>
2022-12-09 17:18:17 +0530

    Finished 10 programs.

--- src/test/java/hundredjavaprogramming/Aroon/FindLargestNumber.java
+++ src/test/java/hundredjavaprogramming/Aroon/FindLargestNumber.java
@@ -1,17 +1,33 @@
 package hundredjavaprogramming.Aroon;
 
+import java.util.Scanner;
+
 public class FindLargestNumber {
-	
-	public static void getLargestNumber(int[] arr) {
-		
-		
-		
+
+	public static void getLargestNumber() {
+
+		System.out.println("Enter the total number of Elements");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int temp = 0;
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter the number for index_" + i);
+			a[i] = sc.nextInt();
+		}
+		for (int i = 0; i < a.length; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.println("The largest number is: " + a[a.length - 1]);
 	}
 
 	public static void main(String[] args) {
-
-		
-		
+		getLargestNumber();
 	}
-
 }
---
+++ src/test/java/hundredjavaprogramming/Aroon/IfElse.java
@@ -0,0 +1,34 @@
+package hundredjavaprogramming.Aroon;
+
+import java.util.Scanner;
+
+public class IfElse {
+	
+	
+	
+	public static String greet() {
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please enter your name");
+		String name = scn.next();
+		System.out.println("Say Hi or Hello");
+		
+		String str = scn.next();
+		if (str.equalsIgnoreCase("hi")) {
+			
+			return "Hello "+name;
+			
+		}else if (str.equalsIgnoreCase("hello")) {
+			return "Hi name"+name;
+		}else {
+			return "Please say Hi or Hello";
+		}
+	}
+	
+public static void main(String[] args) {
+	System.out.println(greet());
+	
+}
+	
+
+}
