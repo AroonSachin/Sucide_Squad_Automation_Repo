fdd88e1fbe0493f9e73f8ac91871bf2e94a6c79a
amaduraiveeran <amaduraiveeran@TSQA-L048.ittrident.local>
2023-01-24 11:59:55 +0530

    Added Package.json file
    Updated .gitignore
    
    Finished Programs- 13 & 14.

---
+++ src/test/java/hundred_js_programming/Aroon/Prog-13.js
@@ -0,0 +1,13 @@
+/**
+ * @Question How to check Odd and Even Number
+ */
+
+function OorEFinder(num){
+	if(num % 2 === 0 ){
+		console.log(`${num} is an Even number`)
+	}else{
+		console.log(`${num} is an Odd number`)
+	}
+}
+
+OorEFinder(3)
